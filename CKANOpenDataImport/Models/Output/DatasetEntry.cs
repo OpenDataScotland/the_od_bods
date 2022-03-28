783271060b007e1ac7366b83e051d728f7fa3679
Jack Gilmore <gilmorejackc@gmail.com>
2022-03-28 19:20:56 +0100

    Add FileName to CKAN output

--- CKANOpenDataImport/Models/Output/DatasetEntry.cs
+++ CKANOpenDataImport/Models/Output/DatasetEntry.cs
@@ -8,6 +8,7 @@
         public string AssetURL { get; set; }
         public string DateCreated { get; set; }
         public string DateUpdated { get; set; }
+        public string FileName { get; set; }
         public string FileSize { get; set; }
         public string FileSizeUnit { get; set; }
         public string FileType { get; set; }
