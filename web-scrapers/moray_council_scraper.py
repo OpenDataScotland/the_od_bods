122cfb0d57f1caad99e7ce6c31c4f97f41b993bc
Hariss Ali Gills <harissgills@gmail.com>
2022-05-22 10:05:59 +0100

    started working on moray_council scraper

---
+++ web-scrapers/moray_council_scraper.py
@@ -0,0 +1,72 @@
+# Packages: beautifulsoup4, csv, requests
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+URL_COUNCIL = "https://data-moray.opendata.arcgis.com/datasets/moray::"
+URL_PAGE = ""
+
+def get_headers():
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '3600',
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+        }
+    return headers    
+
+def get_all_files():
+    url = URL_COUNCIL+URL_PAGE 
+    req = requests.get(url, get_headers())
+    soup = BeautifulSoup(req.content, 'html.parser')
+    list_of_h1_tags = soup.find_all("h1")
+    print("h1 tags: " + str(list_of_h1_tags))
+    list_of_files = []
+    for poss in list_of_h1_tags:
+        list_of_files.append(poss)
+
+    return list_of_files        
+
+
+def csv_file_metadata(file_loc):
+    text = requests.get(URL_COUNCIL+file_loc , get_headers()).text
+    lines = text.splitlines()
+    data = csv.reader(lines)
+    number_of_records = len(list(data))-1
+
+    total_bytes = -1
+    for line in lines:
+        bytes_on_this_line = len(line) + 1
+        total_bytes += bytes_on_this_line
+
+
+    return number_of_records, total_bytes
+    
+
+def csv_output(header, data): 
+
+    with open('the_od_bods/web-scrapers/output.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+
+    # write the header
+        writer.writerow(header)
+
+    # write the data
+        for record in data:
+            writer.writerow(record)
+
+if __name__ == "__main__":
+    URL_PAGE = str(input("Enter the the url after the two \"::\"" ))
+    get_all_files()  
+    # Record Headings
+    header = ["Title","Owner","PageURL","AssetURL","DateCreated","DateUpdated","FileSize","FileSizeUnit","FileType","NumRecords","OriginalTags","ManualTags","License","Description"]
+    data = []
+
+    list_of_files = get_all_files()
+    for fi in list_of_files:
+        metadata = csv_file_metadata(fi['href'])
+        output = [fi.string, "East Ayrshire Council", URL_COUNCIL+URL_PAGE, URL_COUNCIL+fi['href'], "NULL", "NULL", metadata[1], "B", "CSV", "B", metadata[0], "NULL", "Education", "Open Government Licence 3.0 (United Kingdom)", "NULL"]     
+        data.append(output)
+
+    csv_output(header, data)
