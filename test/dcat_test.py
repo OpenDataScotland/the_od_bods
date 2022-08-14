50aaa2680458101efca86c52ddd072bfd42dcd50
Gavin Barnett <gavbarnett@gmail.com>
2022-08-14 21:44:31 +0100

    Added test for exact csv match
    this test ensures the csv output is the same as expected - (read as the same output as when this test was created,)
    
    Testing:
    ran pytest 100% pass
    22 passed in 1.01s

--- test/dcat_test.py
+++ test/dcat_test.py
@@ -1,5 +1,6 @@
 from wsgiref.validate import validator
 import validators
+import filecmp
 import os
 import dateutil.parser
 import re
@@ -14,12 +15,14 @@ def test_get_datasets():
     owner = "test_owner"
     start_url = "file:///" + os.path.abspath("test/mock_data/dcat/glasgow.json")
     fname = "test/mock_data/output/dcat/glasgow.csv"
+    expected_fname = "test/mock_data/dcat/expected/glasgow.csv"
     if os.path.exists(fname):
         os.remove(fname)
     test_proc.get_datasets(owner, start_url, fname)
     with open(fname, "r", newline="") as check_file:
         csv_check_file = csv.reader(check_file)
         assert csv_checker(csv_check_file)
+    assert filecmp.cmp(fname, expected_fname)
 
 
 def is_valid_string(str_to_check):
