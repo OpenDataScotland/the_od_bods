53b29af446a4a84b0c90df93e8c40483c1222b49
nutcracker22 <nutcracker@mailbox.org>
2022-09-12 14:50:00 +0100

    restored merge_data.py to the current version of OpenDataScotland:main

--- merge_data.py
+++ merge_data.py
@@ -672,6 +672,7 @@ def clean_data(dataframe):
 
     data["License"] = data["License"].apply(tidy_licence)
 
+<<<<<<< HEAD
 
     def tidy_file_type(file_type):
         """ Temporary data type conversion
