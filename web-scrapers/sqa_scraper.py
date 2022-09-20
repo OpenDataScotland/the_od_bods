5bc69ab1aea50ff1e91fb6d905a8ebc0a72c9b7d
nutcracker22 <nutcracker@mailbox.org>
2022-09-20 21:32:23 +0100

    finished fetch_datasets, create_title and fetch_asset_url functions

--- web-scrapers/sqa_scraper.py
+++ web-scrapers/sqa_scraper.py
@@ -66,20 +66,78 @@ def fetch_available_years():
     for dropdown_item in dropdown_list:
         # print("dropdown item", dropdown_item, type(dropdown_item), dropdown_item["value"])
         dict_of_links[dropdown_item.contents[0]] = "https://www.sqa.org.uk/sqa/" + dropdown_item["value"]
-    print(dict_of_links)
-    keys_to_skip = ["--- Select Year ---", "Statistics archive"]
+    # print(dict_of_links)
+    keys_to_skip = ["--- Select Year ---", "Statistics archive", "Related", "Derived Grades 2003"]
     [dict_of_links.pop(d) for d in keys_to_skip]
-    print(dict_of_links)
+    # print(dict_of_links)
 
     return dict_of_links
 
 
-def fetch_year_page(link: str) -> str:
+def fetch_year_page(link: str) -> BeautifulSoup:
+    """
+    Fetches the content of pages of available years.
+
+    Returns:
+        BeautifulSoup object of the pages.
+    """
     req = requests.get(link, get_headers())
     return BeautifulSoup(req.content, "html.parser")
 
 
-def
+def fetch_datasets(page: BeautifulSoup) -> list:
+    """
+    Fetches the datasets present on a page.
+
+    Returns:
+        list_of_lists_of_datasets (List): A list of datasets present on the specific page.
+    """
+    list_of_lists_of_datasets = []
+    # h1_headline = page.h1.descendants
+    # print(h1_headline)
+    # unsorted_lists = h1_headline.find_all("ul")
+    content = page.find(id="content")
+    # print(len(content), content)
+    unsorted_lists = content.find_all("ul")
+    for unsorted_list in unsorted_lists:
+        # print("unsorted_list ", unsorted_list)
+        list_of_datasets = unsorted_list.find_all("a")
+        list_of_lists_of_datasets.append(list_of_datasets)
+    # print("list_of_datasets ", list_of_lists_of_datasets)
+
+    return list_of_lists_of_datasets
+
+
+def create_title(part1: str, part2: BeautifulSoup) -> str:
+    """
+    Combines the two inputs into the title of the dataset
+
+    Returns:
+        stripped_title (str): A string of dataset's title.
+    """
+    title_string1 = year + " " + dataset.get_text()
+    stripped_title = title_string1.split(" ", 1)[1]
+    return stripped_title
+
+
+def fetch_asset_url(page: BeautifulSoup) -> str:
+    """
+    Fetches the asset url of the dataset from the BeautifulSoup object.
+
+    Returns:
+        url (str): A string of dataset's asset url.
+    """
+    link = page.get("href")
+    if link.startswith("//"):
+        url = "https:" + link
+    elif link.startswith("files"):
+        url = "https://www.sqa.org.uk/sqa/" + link
+    elif link.startswith("/sqa"):
+        url = "https://www.sqa.org.uk" + link
+    else:
+        url = link
+
+    return url
 
 
 if __name__ == "__main__":
@@ -109,45 +167,58 @@ if __name__ == "__main__":
         years_page = fetch_year_page(category_links[year])
         owner = "Scottish Qualifications Authority (SQA)"
         pageurl = category_links[year]
+        list_datasets = fetch_datasets(years_page)
+        for list in list_datasets[:-3]:
+            print("list", list)
+            for dataset in list:
+                print("dataset", dataset)
+                title = create_title(year, dataset)
+                print("title", title)
+                asset_url = fetch_asset_url(dataset)
 
-        title = "NULL"
-        asset_url = "NULL"
-        create_date = "NULL"
-        file_size = "NULL"
-        file_unit = "NULL"
-        data_type = "NULL"
-        num_recs = "NULL"
-        sqa_licence = "NULL"
 
-        """
-        title = fetch_title(soup)
-        # print("title:", title)
-        # print("pageurl:", pageurl)
-        asset_url = fetch_asset_url(soup)
-        # print("asset_url:", asset_url)
-        """
+                create_date = "NULL"
+                file_size = "NULL"
+                file_unit = "NULL"
 
-        output = [
-            title,
-            owner,
-            pageurl,
-            asset_url,
-            create_date,
-            "NULL",
-            file_size,
-            file_unit,
-            data_type,
-            num_recs,
-            "NULL",
-            "NULL",
-            sqa_licence,
-            "NULL",
-        ]
-        data.append(output)
+                data_type = dataset.get("href").split(".")[1]
+                print(data_type)
+                num_recs = "NULL"
+                sqa_licence = "NULL"
+
+                """
+                title = fetch_title(soup)
+                # print("title:", title)
+                # print("pageurl:", pageurl)
+                asset_url = fetch_asset_url(soup)
+                # print("asset_url:", asset_url)
+                """
+
+                output = [
+                    title,
+                    owner,
+                    pageurl,
+                    asset_url,
+                    create_date,
+                    "NULL",
+                    file_size,
+                    file_unit,
+                    data_type,
+                    num_recs,
+                    "NULL",
+                    "NULL",
+                    sqa_licence,
+                    "NULL",
+                ]
+                data.append(output)
 
     print("Outputting to CSV")
     csv_output(header, data)
 
 
-
+"""
+issues with this scraper:
+- year before 2000 don't return a dataset, even though there are unordered lists present"
+- in many (all?) years, the last datasets seem to be missing
+"""
 
