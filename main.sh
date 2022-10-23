3e3eb6da322f93f9bc58b2731374291155e50072
Karen Jewell <kar.jewell@gmail.com>
2022-10-23 23:18:41 +0100

    feat: added shell script for entire pipeline

---
+++ main.sh
@@ -0,0 +1,24 @@
+# to run, in terminal: sh main.sh
+# clear folders
+find data/arcgis/ -type f -delete
+find data/ckan/ -type f -delete
+find data/dcat/ -type f -delete
+find data/scraped-results/ -type f -delete
+find data/USMART/ -type f -delete
+find data/merged_output.csv -type f -delete
+# run source scripts
+python arcgis.py
+python usmart.py
+python ckan.py
+python sparkql_statistics.py
+python dcat.py
+cd web-scrapers
+python aberdeenshire_council_scraper.py
+python east_ayrshire_scraper.py
+python moray_council_scraper.py
+python nls_scraper.py
+cd ..
+# processing
+python merge_data.py
+python export2jkan.py
+echo "main.sh complete"
