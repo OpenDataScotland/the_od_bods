a573f8714f02ca0cd5b05afaef7b9a3e589cda16
Jack Gilmore <46202639+JackGilmore@users.noreply.github.com>
2022-11-04 10:54:10 +0000

    Fix log path confusion

--- processor.py
+++ processor.py
@@ -57,9 +57,9 @@ class Processor:
                 'error_code': "",
                 'error_reason': str(err2.reason),
                 }
-        with open('../opendata.scot_pipeline/log.json', 'a') as f:
+        with open('log.json', 'a') as f:
             json.dump(error_dict, f)
-        with open('../opendata.scot_pipeline/log.md', 'a') as file:
+        with open('log.md', 'a') as file:
             file.write(f'| {error_dict["url"]} | {error_dict["error_code"]} | {error_dict["error_reason"]} | \n')
 
         return "NULL"
