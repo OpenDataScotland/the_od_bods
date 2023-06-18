af49bd88975d558a7aceb26d1778ad30cf6ccdd4
Jack Gilmore <46202639+JackGilmore@users.noreply.github.com>
2023-06-18 12:39:16 +0100

    Reformat using black

--- dcat.py
+++ dcat.py
@@ -18,7 +18,6 @@ class ProcessorDCAT(Processor):
         print(start_url)
         datasets_collection = processor.get_json(start_url)
         if datasets_collection != "NULL":
-
             datasets_collection = datasets_collection["dcat:dataset"]
 
             print(f"Found {len(datasets_collection)} datasets")
@@ -34,26 +33,23 @@ class ProcessorDCAT(Processor):
 
                 # Get common metadata
                 title = dataset.get("dct:title", "")
-                owner = dataset.get("dct:publisher", "").get("foaf:name","")
-                original_dataset_link = dataset.get("@id","")
+                owner = dataset.get("dct:publisher", "").get("foaf:name", "")
+                original_dataset_link = dataset.get("@id", "")
                 date_created = parser.parse(dataset.get("dct:issued", "")).date()
                 date_modified = parser.parse(dataset.get("dct:modified", "")).date()
-                tags = ";".join(
-                        map(str, keywords)
-                    )
-                
-                metadata_url= dataset.get("dct:identifier","")
-                license = get_license(metadata_url) # TODO
+                tags = ";".join(map(str, keywords))
+
+                metadata_url = dataset.get("dct:identifier", "")
+                license = get_license(metadata_url)  # TODO
 
                 description = dataset.get("dct:description", "").strip("\u200b")
 
                 dataset_resources = dataset.get("dcat:distribution")
 
                 for resource in dataset_resources:
-                    resource_link = resource.get("dcat:accessURL",{}).get("@id","")
-                    file_name = resource.get("dct:description","")
-                    file_type = resource.get("dct:title","")
-
+                    resource_link = resource.get("dcat:accessURL", {}).get("@id", "")
+                    file_name = resource.get("dct:description", "")
+                    file_type = resource.get("dct:title", "")
 
                     prepped_resource = [
                         title,
@@ -68,11 +64,11 @@ class ProcessorDCAT(Processor):
                         file_type,
                         "",  # TODO: numrecords,
                         tags,
-                        "", # Manual tags
+                        "",  # Manual tags
                         license,
-                        description
-                        ]
-                    
+                        description,
+                    ]
+
                     prepped.append(prepped_resource)
 
             print(f"{len(prepped)} lines for csv")
@@ -81,35 +77,64 @@ class ProcessorDCAT(Processor):
 
 def get_license(metadata_url):
     parsed_url = urlparse(metadata_url)
-    dataset_guid = parse_qs(parsed_url.query)['id'][0]
+    dataset_guid = parse_qs(parsed_url.query)["id"][0]
 
-    license_metadata_url = f"https://www.arcgis.com/sharing/rest/content/items/{dataset_guid}?f=json"
+    license_metadata_url = (
+        f"https://www.arcgis.com/sharing/rest/content/items/{dataset_guid}?f=json"
+    )
 
     license_metadata = processor.get_json(license_metadata_url)
 
-    return parse_license(license_metadata.get("licenseInfo",""))
+    return parse_license(license_metadata.get("licenseInfo", ""))
+
 
 # TODO: This probably needs refactored as part of a main license parsing function
 def parse_license(license_info):
     if license_info in (None, ""):
         return ""
-    
+
     # Strip HTML
     soup = BeautifulSoup(license_info)
-    stripped_license_info = soup.get_text().replace("\t", " ").replace("\r", " ").replace("\n", " ")
+    stripped_license_info = (
+        soup.get_text().replace("\t", " ").replace("\r", " ").replace("\n", " ")
+    )
 
     if stripped_license_info in (None, ""):
         return ""
 
-    if any(s.lower() in stripped_license_info.lower() for s in ("Data is being released under Open Government Licence terms", "supplied under the Open Government Licence", "supplied under the Open Government License", "supplied under the Open Government License", "This dataset is available for use under the Open Government Licence", "This dataset is available under the terms of the UK Open Government Licence","http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3","made available under the Open Government Licence", "you do have to adhere to the terms of the Open Government Licence", "We, Stirling Council, publish our mapping datasets under the Open Government Licence", "publish our mapping datasets under the Open Government Licence", "We use the Open Government Licence")):
+    if any(
+        s.lower() in stripped_license_info.lower()
+        for s in (
+            "Data is being released under Open Government Licence terms",
+            "supplied under the Open Government Licence",
+            "supplied under the Open Government License",
+            "supplied under the Open Government License",
+            "This dataset is available for use under the Open Government Licence",
+            "This dataset is available under the terms of the UK Open Government Licence",
+            "http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3",
+            "made available under the Open Government Licence",
+            "you do have to adhere to the terms of the Open Government Licence",
+            "We, Stirling Council, publish our mapping datasets under the Open Government Licence",
+            "publish our mapping datasets under the Open Government Licence",
+            "We use the Open Government Licence",
+        )
+    ):
         return "OGL3"
-    
-    if stripped_license_info.lower().startswith("open government licence") or stripped_license_info.lower().startswith("open government license") or stripped_license_info.lower().startswith("uk open government licence") or stripped_license_info.lower().startswith("uk open government license"):
+
+    if (
+        stripped_license_info.lower().startswith("open government licence")
+        or stripped_license_info.lower().startswith("open government license")
+        or stripped_license_info.lower().startswith("uk open government licence")
+        or stripped_license_info.lower().startswith("uk open government license")
+    ):
         return "OGL3"
-    
-    if "http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3" in stripped_license_info.lower():
+
+    if (
+        "http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3"
+        in stripped_license_info.lower()
+    ):
         return "OGL3"
-    
+
     if stripped_license_info == "CC-BY-SA":
         return "https://creativecommons.org/licenses/by-sa/3.0/"
 
@@ -117,7 +142,6 @@ def parse_license(license_info):
     print(f"UNKNOWN LICENSE: {stripped_license_info}")
 
     return ""
-
 
 
 processor = ProcessorDCAT()
